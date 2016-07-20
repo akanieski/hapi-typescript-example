a7a09f5efbda19f15f324c69d152af2a053b6cda
Andrew Kanieski <andrew.legacy@gmail.com>
2016-07-19 22:50:20 -0400

    Updated typings to latest version

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-/// <reference path="../typings.d.ts" />
+/// <reference path="../typings/index.d.ts" />
+
 import {IPlugin} from "./libs/plugins/interfaces";
 import Configurations from "./configs/configurations";
-import * as Hapi from "hapi";
+import Hapi = require('hapi');
 import Routes from "./routes";
-const fs = require('fs');
-const path = require('path');
+import fs = require('fs');
+import path = require('path');
 
 var port = process.env.port || Configurations.Server.port;
 var server = new Hapi.Server();
@@ -16,7 +17,7 @@ const pluginsPath = __dirname + '/libs/plugins/';
 const plugins = fs.readdirSync(pluginsPath).filter(file => fs.statSync(path.join(pluginsPath, file)).isDirectory());
 
 plugins.forEach((pluginName: string) => {
-    var plugin: IPlugin = (require("./libs/plugins/" + pluginName)).default();      
+    var plugin: IPlugin = (require("./libs/plugins/" + pluginName)).default();
     console.log(`Register Plugin ${plugin.info().name} v${plugin.info().version}`);
     plugin.register(server);
 });
